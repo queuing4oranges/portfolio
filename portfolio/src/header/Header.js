597fe9a1700d1e590732989657d921b6ed12d3bd
queuing4oranges <katjazenker@gmail.com>
2022-08-01 18:28:16 +0200

    start portfolio

---
+++ portfolio/src/header/Header.js
@@ -0,0 +1,18 @@
+const Header = () => {
+    
+    
+    
+    
+    return ( 
+        <div className="header__cont">
+            <div className="header__home">Home</div>
+            <div className="header__skills">Skills</div>
+            <div className="header__about">About</div>
+            <div className="header__contact">Contact</div>
+        </div>
+
+
+     );
+}
+ 
+export default Header;
