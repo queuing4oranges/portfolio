669c4dfa1f799733d993e944bc54685a5dce0668
queuing4oranges <katjazenker@gmail.com>
2024-03-17 21:40:29 +0100

    Fixes in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-scroll";
 import { FaHamburger } from "react-icons/fa";
 import { BsChevronDoubleUp } from "react-icons/bs";
 
 import "./navbar.scss";
 
+const links = [
+	{ to: "projects", label: "Projects" },
+	{ to: "skills", label: "Skills" },
+	{ to: "about", label: "About" }
+]
+
 const Navbar = () => {
 	const [isOpen, setIsOpen] = useState(false);
+	const [isScrolled, setIsScrolled] = useState(false);
+	const [isAtTop, setIsAtTop] = useState(false);
 	
+	useEffect(() => {
+		// Change navbar to symbols when scrolling
+		setIsScrolled(window.scrollY > 0);
+		setIsAtTop(window.scrollY === 0);
+
+		function handleScroll() {
+			const scrolled = window.scrollY > 0;
+			const atTop = window.scrollY === 0;
+			const navbar = document.querySelector(".nav-content")
+
+			if (scrolled) {
+				navbar.classList.add("animated");
+			} else if (atTop) {
+				navbar.classList.remove("animated");
+			}
+		}
+
+		window.addEventListener('scroll', handleScroll);
+
+		// Clean up on unmount
+		return () => {
+			window.removeEventListener('scroll', handleScroll)
+		};
+	},[]);
+
 	const toggleMenu = () => {
 		setIsOpen(!isOpen);
 	}
@@ -20,7 +53,7 @@ const Navbar = () => {
 						<FaHamburger color="#ebd2c1" size={30} />
 					</button>
 				</div>
-				<div className={`md:flex ${isOpen ? 'mt-12 flex-col md:flex-row' : 'hidden'} ${isOpen ? 'bg-dark rounded-md' : ''}`}>
+				<div className={`nav-content md:flex ${isOpen ? 'mt-12 flex-col md:flex-row bg-dark rounded-md' : 'hidden'} ${isOpen ? '' : ''}`}>
 					<button className="cv-download bg-transparent text-accent text-xs py-2 px-4 rounded-full hover:bg-hover hover:text-accent">
 						<a
 							href="KatjaZenkerCV.pdf"
@@ -28,7 +61,7 @@ const Navbar = () => {
 							rel="noreferrer"
 							download="KatjaZenkerCV.pdf"
 						>
-							Download CV
+							<p>Download CV</p>
 						</a>
 					</button>
 					{isOpen ?
@@ -44,35 +77,18 @@ const Navbar = () => {
 					: null
 					}
 
-					<Link 
-						to="projects" 
-						smooth={true} 
-						spy={true} 
-						activeClass="active" 
-						className={`flex items-center ml-8 hover:text-accent ${isOpen ? "text-xl ml-0 px-3" : "" }`}
-						onClick={() => setIsOpen(false)}>
-						Projects
-					</Link>
-					
-					<Link 
-						to="skills" 
-						smooth={true} 
-						spy={true} 
-						activeClass="active" 
-						className={`flex items-center ml-8 hover:text-accent ${isOpen ? "text-xl ml-0 px-3" : ""}`}
-						onClick={() => setIsOpen(false)}>
-						Skills
-					</Link>
-					
-					<Link 
-						to="about" 
-						smooth={true} 
-						spy={true} 
-						activeClass="active" 
-						className={`flex items-center ml-8 hover:text-accent ${isOpen ? "text-xl ml-0 px-3" : ""}`} 
-						onClick={() => setIsOpen(false)}>
-						About
-					</Link>
+					{links.map((link, index) => (
+						<Link
+							key={index}
+							to={link.to}
+							smooth={true}
+							spy={true}
+							activeClass="active"
+							className={`flex items-center ml-8 hover:text-accent ${isOpen ? "text-xl ml-0 px-3" : "" }`}
+							onClick={() => setIsOpen(false)}>
+							<p>{link.label}</p>
+						</Link>
+					))}
 					
 				</div>
 			</div>
