999f2d9a84620fb3fcec08d215d82d558f368574
queuing4oranges <katjazenker@gmail.com>
2024-03-21 22:58:09 +0100

    Set up animate in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,34 +2,50 @@ import { useEffect, useState } from "react";
 import { Link } from "react-scroll";
 import { FaHamburger } from "react-icons/fa";
 import { BsChevronDoubleUp } from "react-icons/bs";
+import { GoPerson, GoRocket, GoTools } from "react-icons/go";
 
 import "./navbar.scss";
 
 const links = [
-	{ to: "projects", label: "Projects" },
-	{ to: "skills", label: "Skills" },
-	{ to: "about", label: "About" }
+	{ to: "projects", label: "Projects", icon: <GoRocket size={40} color="#00C59B" /> },
+	{ to: "skills", label: "Skills", icon: <GoTools size={40} color="#00C59B" /> },
+	{ to: "about", label: "About", icon: <GoPerson size={40} color="#00C59B" /> }
 ]
 
 const Navbar = () => {
 	const [isOpen, setIsOpen] = useState(false);
-	const [isScrolled, setIsScrolled] = useState(false);
-	const [isAtTop, setIsAtTop] = useState(false);
+	const [isWindowSizeSmall, setIsWindowSizeSmall] = useState(false);
+	const [isAnimated, setIsAnimated] = useState(true);
+
+	useEffect(() => {
+		function handleResize() {
+			setIsWindowSizeSmall(window.innerWidth < 768);
+		}
+
+		handleResize();
+		window.addEventListener('resize', handleResize);
+
+		// Clean up function
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		}
+	}, [window.innerWidth])
 	
 	useEffect(() => {
 		// Change navbar to symbols when scrolling
-		setIsScrolled(window.scrollY > 0);
-		setIsAtTop(window.scrollY === 0);
+		const navbar = document.querySelector(".nav-content")
 
 		function handleScroll() {
-			const scrolled = window.scrollY > 0;
 			const atTop = window.scrollY === 0;
-			const navbar = document.querySelector(".nav-content")
 
-			if (scrolled) {
-				navbar.classList.add("animated");
-			} else if (atTop) {
-				navbar.classList.remove("animated");
+			if (!isWindowSizeSmall) {
+				if (atTop) {
+					navbar.classList.remove("animated");
+					setIsAnimated(false);
+				} else {
+					navbar.classList.add("animated");
+					setIsAnimated(true);
+				}
 			}
 		}
 
@@ -48,12 +64,14 @@ const Navbar = () => {
 	return (
 		<nav className="nav-container fixed top-0 z-50 w-full bg-transparent text-light py-4 px-6 md:px-40">
 			<div className="flex justify-end items-center">
+				{/* Show hamburger menu, when small, hide on desktop */}
 				<div className="block md:hidden">
 					<button onClick={toggleMenu} className="hamburger-menu text-white focus:outline-none absolute right-4 top-4 p-2">
 						<FaHamburger color="#ebd2c1" size={30} />
 					</button>
 				</div>
-				<div className={`nav-content md:flex ${isOpen ? 'mt-12 flex-col md:flex-row bg-dark rounded-md' : 'hidden'} ${isOpen ? '' : ''}`}>
+				{/* Show menu on toggle */}
+				<div className={`nav-content md:flex ${isOpen ? 'mt-12 flex-col md:flex-row bg-dark rounded-md' : 'hidden'}`}>
 					<button className="cv-download bg-transparent text-accent text-xs py-2 px-4 rounded-full hover:bg-hover hover:text-accent">
 						<a
 							href="KatjaZenkerCV.pdf"
@@ -64,6 +82,7 @@ const Navbar = () => {
 							<p>Download CV</p>
 						</a>
 					</button>
+					{/* Display Chevron when toggle is open and go back up */}
 					{isOpen ?
 						<Link
 							to="home"
@@ -76,7 +95,7 @@ const Navbar = () => {
 						</Link>
 					: null
 					}
-
+					{/* Map through nav links */}
 					{links.map((link, index) => (
 						<Link
 							key={index}
