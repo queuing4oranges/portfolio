8a480cedc4c1f66bdbea3397a05031cc427223d0
queuing4oranges <katjazenker@gmail.com>
2024-04-24 21:11:07 +0200

    Refactor font size and position in hero, remove animation

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,15 +8,15 @@ const Hero = () => {
 
 	return (
 	<section
-		className="hero-container relative w-full h-400 overflow-hidden"
+		className="hero-container relative w-screen h-400 overflow-hidden"
 		id="home">
 			<div className="hero-image h-full w-full flex flex-col items-center justify-center text-center">
-				<h1 className="hero-title text-light text-xl md:text-3xl sm:p-4">Hi, I'm Katja</h1>
-				<h2 className="hero-text text-light text-xl md:text-2xl sm:p-4">a Prague based Web Developer</h2>
+				<h1 className="hero-title text-light text-xl md:text-3xl text-center leading-tight">Hi, I'm Katja</h1>
+				<h2 className="hero-text text-light text-md md:text-2xl sm:p-4">a Prague based Web Developer</h2>
 				<button
 					type="button"
 					onClick={() => setOpenModal(!openModal)}
-					className="bg-accent text-dark text-sm md:text-md py-2 px-4 mt-5 rounded-sm hover:bg-hover hover:text-accent"
+					className="bg-accent text-dark text-sm md:text-md py-2 mt-20 px-4 mt-5 rounded-sm hover:bg-hover hover:text-accent"
 				>
 				Get in touch
 				</button>
